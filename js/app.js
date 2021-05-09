cbd58e8ae5cf7d430020c1ab045a195dff0b5465
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-05-08 20:19:50 -0400

    created trace and x/y values for bar graph

--- js/app.js
+++ js/app.js
@@ -1,2 +1,29 @@
-d3.json("./data/samples.json");
+d3.json("./data/samples.json").then((data) => {
+    dropDown(data.names)
+});
 
+
+// Populates the dropdown with subject ID
+function dropDown(ID) {
+    let options = d3.select("#selDataset");
+        ID.forEach((id) => {
+            let option = options.append("option");
+            option.text(id).property("value", id)
+        })
+    };
+
+function barPlot(subject) {
+    var xValue = subject.sample_values.slice(0, 10);
+    var yValue = subject.otu_ids.slice(0, 10).map(otu => `OTU ${otu_id}`);
+    var hoverText = subject.otu_labels.slice(0, 10)
+    let trace1 = {
+        type: "bar",
+        x: xValue,
+        y: yValue,
+    }
+    var data = [trace1]
+    var layout = {
+        title: "Top 10 Bacteria Cultures Found"
+    }
+
+};
