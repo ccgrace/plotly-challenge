36322de540b1e72d06a73400bfd0664424cea098
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-05-08 22:01:10 -0400

    completed bubble plot, doesn't work, don't know why

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 d3.json("./data/samples.json").then((data) => {
     dropDown(data.names)
+    barPlot(data.samples[0])
+    bubblePlot(data.samples[0])
 });
 
 
@@ -12,18 +14,45 @@ function dropDown(ID) {
         })
     };
 
+// Creates Barplot using slice method to limit to top 10 values
 function barPlot(subject) {
     var xValue = subject.sample_values.slice(0, 10);
-    var yValue = subject.otu_ids.slice(0, 10).map(otu => `OTU ${otu_id}`);
+    var yValue = subject.otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`);
     var hoverText = subject.otu_labels.slice(0, 10)
     let trace1 = {
         type: "bar",
-        x: xValue,
-        y: yValue,
+        x: xValue.reverse(),
+        y: yValue.reverse(),
+        text: hoverText,
+        orientation: "h",
     }
     var data = [trace1]
     var layout = {
         title: "Top 10 Bacteria Cultures Found"
     }
+    Plotly.newPlot("bar", data, layout);
+};
 
+function bubblePlot(subject) {
+    var xValue = subject.otu_ids;
+    var yValue = subject.sample_values;
+
+    let trace2 = {
+        x: xValue,
+        y: yValue,
+        text: subject.otu_labels
+        mode: "markers",
+        marker: {
+            color: subject.otu_ids,
+            size: subject.sample_values,
+        };
+    };
+
+    var data = [trace2]
+
+    var layout = {
+        title: "Bacteria Cultures Per Sample"
+    };
+
+    Plotly.newPlot("bubble", data, layout);
 };
