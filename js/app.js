54feee82c446e464934b1116d48fb5eb9a5ddf27
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-05-08 23:38:18 -0400

    completed update functions

--- js/app.js
+++ js/app.js
@@ -3,6 +3,17 @@ d3.json("./data/samples.json").then((data) => {
     barPlot(data.samples[0]);
     bubblePlot(data.samples[0]);
     metaData(data.metadata[0])
+
+    let selectedSubject = d3.select("#selDataset");
+    selectedSubject.on("change",function() {
+    var subject = selectedSubject.property("value")
+    var sbjIndex = data.names.indexOf(subject)
+
+    dropDown(data.names);
+    barPlot(data.samples[sbjIndex]);
+    bubblePlot(data.samples[sbjIndex]);
+    metaData(data.metadata[sbjIndex])
+    })
 });
 
 
@@ -65,14 +76,4 @@ function metaData(subject) {
     Object.entries(subject).forEach(([key, value])=> {
         let metaData = sample_metaData.append("p").text(`${key}: ${value}`);
     });
-}
-
-// Creates an on-change function to perform data functions based on chosen Subject ID
-let selectedSubject = d3.select("#selDataset");
-selectedSubject.on("change",function() {
-    var subject = selectedSubject.property("value")
-    // console.log(subject)
-});
-
-let sbjIndex = data.names.indexOf(subject);
-console.log(sbjIndex);
+};
